e3b13738cca7f4bcf4edc8f391af8c52358c0d66
20039761 <20039761@LAPTOP-6JQ3V56H>
2021-08-02 17:35:46 +0800

    class

--- C206_CaseStudy/src/Feedback.java
+++ C206_CaseStudy/src/Feedback.java
@@ -8,5 +8,38 @@
  */
 
 public class Feedback {
+	private String sat;
+	private String issue;
+	private String status;
+	
+	public Feedback(String sat, String issue, String status) {
+		this.sat = sat;
+		this.issue = issue;
+		this.status = status;
+	}
 
+	public String getSat() {
+		return sat;
+	}
+
+	public void setSat(String sat) {
+		this.sat = sat;
+	}
+
+	public String getIssue() {
+		return issue;
+	}
+
+	public void setIssue(String issue) {
+		this.issue = issue;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 }
