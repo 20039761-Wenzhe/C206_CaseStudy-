9c6b9b11d39fd42b47f2e087aa35da96d2d0011c
20039761 <20039761@LAPTOP-6JQ3V56H>
2021-08-03 20:26:19 +0800

    done

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -31,18 +31,106 @@ public class C206_CaseStudy {
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
-				carMenu();
+				System.out.println("1. View Car");
+				System.out.println("2. Add Car");
+				System.out.println("3. Delete Car");
+				
+				int input = Helper.readInt("Enter an option > ");
+			if(input == 1) {
+				viewCar(carList);
+			}
+			if(input == 2) {
+				addCar(carList);
+			}
+			if(input == 2) {
+				deleteCar(carList);
+			}
+			
 			} else if (option == 2) {
-				feedbackMenu(feedbackList, issueList);
-			} else if (option == 3) {
-				accountMenu();
-			} else if (option == 4) {
-				appMenu();
-			} else if (option == 5) {
-				System.out.println("Application closed");
+				System.out.println("1. View");
+				System.out.println("2. Add");
+				System.out.println("3. Delete");
+				
+				int input = Helper.readInt("Enter an option > ");
+				
+				
+				if(input == 1) {
+					System.out.println("1. View Issue");
+					System.out.println("2. View Feedback");
+					int input1 = Helper.readInt("Enter an option > ");
+					if (input1 == 1) {
+						viewIssue(issueList);
+						
+				}
+					else if(input == 2) {
+						viewFeedback(feedbackList);
+					}
+				}
+				if(input == 2) {
+					System.out.println("1. Add Issue");
+					System.out.println("2. Add Feedback");
+					int input1 = Helper.readInt("Enter an option > ");
+					
+					if (input1 == 1) {
+						addIssue(issueList);
+					}
+					else if(input == 2) {
+						addFeedback(feedbackList);
+					}
+				}
+				if(input == 3) {
+					System.out.println("1. Delete Issue");
+					System.out.println("2. Delete Feedback");
+					int input1 = Helper.readInt("Enter an option > ");
+					
+					if(input1 == 1) {
+						deleteIssue(issueList);
+					}
+					else if(input1 == 2) {
+						deleteFeedback(feedbackList);
+					}
+				}
+			}
+			else if (option == 3) {
+				System.out.println("1. View Account");
+				System.out.println("2. Add Account");
+				System.out.println("3. Delete Account");
+				
+				int input = Helper.readInt("Enter an option > ");
+			if(input == 1) {
+				viewAccount(accountList);
+			}
+			if(input == 2) {
+				addAccount(accountList);
+			}
+			if(input == 2) {
+				deleteAccount(accountList);
 			}
 		}
+			else if (option == 4) {
+				System.out.println("1. View Appointment");
+				System.out.println("2. Add Appointment");
+				System.out.println("3. Delete Appointment");
+				
+				int input = Helper.readInt("Enter an option > ");
+			if(input == 1) {
+				viewAppointment(appList);
+			}
+			if(input == 2) {
+				addAppointment(appList);
+			}
+			if(input == 2) {
+				deleteAppointment(appList);
+			}
+		}
+			else if(option == 5) {
+				System.out.println("Application closed!");
+			}
+		}
+			
 	}
+	
+	
 
 	private static void menu() {
 		Helper.line(80, "=");
@@ -55,84 +143,83 @@ public class C206_CaseStudy {
 		System.out.println("5. Quit");
 	}
 
-	private static void carMenu() {
+	private static void viewCar(ArrayList<Car> carList) {
+		// jasmine write code here
+		
+	}
+	private static void addCar(ArrayList<Car> carList) {
+		// jasmine write code here
+		
+	}
+	private static void deleteCar(ArrayList<Car> carList) {
 		// jasmine write code here
 		
 	}
 
-	private static void feedbackMenu(ArrayList<Feedback> feedbackList, ArrayList<Issue> issueList){
+	private static void viewIssue(ArrayList<Issue> issueList){
 		// wenzhe write code here
-		System.out.println("1. View");
-		System.out.println("2. Add");
-		System.out.println("3. Delete");
-		int input = Helper.readInt("Enter an option > ");
-		
 		String output = String.format("%-5s %-15s %-35s %-10s\n", "No.", "Name", "Issue", "Status");
-		String output1 = String.format("%-10s %-10s %-15s\n", "No. ", "Name", "Feedback");
 		int counter = 0;
-
-		if (input == 1) {
-			System.out.println("1. View Issue");
-			System.out.println("2. View Feedback");
-			int input1 = Helper.readInt("Enter an option > ");
-			if (input1 == 1) {
 				for(int i =0; i < issueList.size(); i++) {
 					counter = counter +1;
-				output += String.format("%-5d %-15s %-35s %-10s\n", counter, issueList.get(i).getName(), issueList.get(i).getIssue(), issueList.get(i).getStatus());	
-			}
-			System.out.println(output);
-		}
-			if(input1 == 2) {
-				for(int i =0; i < feedbackList.size(); i++) {
-				counter = counter +1;
-				output1 += String.format("%-10d %-10s %-15s\n", counter,feedbackList.get(i).getName(), feedbackList.get(i).getFeedback());	
+				output += String.format("%-5d %-15s %-35s %-10s\n", counter, issueList.get(i).getName(), issueList.get(i).getIssue(), issueList.get(i).getStatus());
+				System.out.println(output);
 				}
-				System.out.println(output1);
-			}
-		}
-		if (input == 2) {
-			System.out.println("1. Add Issue");
-			System.out.println("2. Add Feedback");
-			int input1 = Helper.readInt("Enter an option > ");
-			
-			if (input1 == 1) {
-				String name = Helper.readString("Name > ");
-				String issue = Helper.readString("Enter Issue > ");
-				String status = Helper.readString("Enter Status > ");
-				issueList.add(new Issue(name, issue, status));
-				System.out.println("New Issue added!");
-			}
-			if (input1 == 2) {
-				String name = Helper.readString("Name > ");
-				String feedback = Helper.readString("Enter feedback > ");
-				feedbackList.add(new Feedback(name, feedback));
-				System.out.println("New feedback added!");
-			}
-		
-		}
-		if (input == 3) {
-			System.out.println("1. Delete Issue");
-			System.out.println("2. Delete Feedback");
-			int input1 = Helper.readInt("Enter an option > ");
-			
-			if(input1 == 1) {
-				int number = Helper.readInt("Enter issue no. to delete > ");
-				issueList.remove(number - 1);
-				System.out.println("Issue " + number + " removed");
-			}
-			if (input1 == 2) {
-			int number = Helper.readInt("Enter feedback no. to delete > ");
-			feedbackList.remove(number - 1);
-			System.out.println("Feedback " + number + " removed");
-		}
-		}
-		
+				
 	}
-	private static void accountMenu() {
+	private static void viewFeedback(ArrayList<Feedback> feedbackList){
+		// wenzhe write code here
+		String output1 = String.format("%-10s %-10s %-15s\n", "No. ", "Name", "Feedback");
+		int counter = 0;
+				for(int i =0; i < feedbackList.size(); i++) {
+					counter = counter +1;
+					output1 += String.format("%-10d %-10s %-15s\n", counter,feedbackList.get(i).getName(), feedbackList.get(i).getFeedback());
+				System.out.println(output1);
+				}
+				
+	}
+	private static void addIssue(ArrayList<Issue> issueList){
+		String name = Helper.readString("Name > ");
+		String issue = Helper.readString("Enter Issue > ");
+		String status = Helper.readString("Enter Status > ");
+		issueList.add(new Issue(name, issue, status));
+		System.out.println("New Issue added!");
+	}
+	private static void addFeedback(ArrayList<Feedback> feedbackList){
+		String name = Helper.readString("Name > ");
+		String feedback = Helper.readString("Enter feedback > ");
+		feedbackList.add(new Feedback(name, feedback));
+		System.out.println("New feedback added!");
+	}
+	private static void deleteIssue(ArrayList<Issue> issueList){
+		int number = Helper.readInt("Enter issue no. to delete > ");
+		issueList.remove(number - 1);
+		System.out.println("Issue " + number + " removed");
+	}
+	private static void deleteFeedback(ArrayList<Feedback> feedbackList){
+		int number = Helper.readInt("Enter feedback no. to delete > ");
+		feedbackList.remove(number - 1);
+		System.out.println("Feedback " + number + " removed");
+	}
+	
+			
+	private static void viewAccount(ArrayList<Account> accountList) {
+		// miguel write code here
+	}
+	private static void addAccount(ArrayList<Account> accountList) {
+		// miguel write code here
+	}
+	private static void deleteAccount(ArrayList<Account> accountList) {
 		// miguel write code here
 	}
 
-	private static void appMenu() {
+	private static void viewAppointment(ArrayList<Appointment> appList) {
+		// wenjie write code here
+	}
+	private static void addAppointment(ArrayList<Appointment> appList) {
+		// wenjie write code here
+	}
+	private static void deleteAppointment(ArrayList<Appointment> appList) {
 		// wenjie write code here
 	}
 }
