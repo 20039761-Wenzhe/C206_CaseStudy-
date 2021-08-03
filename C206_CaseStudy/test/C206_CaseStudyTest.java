adc37ff7d477683e67da4310fb11b489f41465dd
20039761 <20039761@LAPTOP-6JQ3V56H>
2021-08-03 21:01:55 +0800

    started test

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,43 @@
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	private Issue i1;
+	private Issue i2;
+	private Feedback f1;
+	private Feedback f2;
+	
+	private ArrayList<Issue> issueList;
+	private ArrayList<Feedback> feedbackList;
 
 	@Before
 	public void setUp() throws Exception {
+		i1 = new Issue("Zhou Ye", "Cashback not given yet", "Pending");
+		i2 = new Issue("Jackey", "Wrong amount charged for the car", "Solving");
+		f1 = new Feedback("Jisoo", "Good customer service");
+		f2 = new Feedback("Shy Chan", "Items are overpriced");
+		
+		issueList = new ArrayList<Issue>();
+		feedbackList = new ArrayList<Feedback>();
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+
 
 	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+	public void testViewIssue() {//wenzhe
+		// Item list is not null, so that can add a new item
+			assertNotNull("Test if there is valid Issue arraylist to add to", issueList);
+	}
+	@Test
+	private void testViewFeedback() {//wenzhe
+		// Item list is not null, so that can add a new item
+			assertNotNull("Test if there is valid Feedback arraylist to add to", feedbackList);
+	}
+	@After
+	public void tearDown() throws Exception {
 	}
 
 }
